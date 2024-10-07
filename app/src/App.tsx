a8b5d7cda84214696e93993cd5df5daf4a1da299
Jeremy Xue <jer1506xue@gmail.com>
2024-10-07 18:35:37 -0500

    add tailwind and file upload

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css';
+import { useState } from 'react';
 import { useWebRTC } from './hooks/useWebRTC';
 
 const App = () => {
@@ -8,13 +8,33 @@ const App = () => {
     }
   });
 
+  const [file, setFile] = useState<File | undefined>(undefined);
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setFile(e.target.files[0]);
+    }
+  };
+
   return (
-    <div className="App">
-      orbit
+    <div className='flex flex-col items-center justify-center min-h-screen'>
       <div>
-        <button onClick={()=>{
-          sendMessage('test');
-        }}> send message </button>
+        <p className='text-4xl'> orbit </p>
+      </div>
+
+      <div>
+          <button onClick={() => sendMessage('test')}>
+            Send Message
+          </button>
+
+        <div>
+          <input 
+            id="file" 
+            type="file" 
+            onChange={handleFileChange} 
+          />
+          {file && <p>Selected File: {file.name}</p>}
+        </div>
       </div>
     </div>
   );
